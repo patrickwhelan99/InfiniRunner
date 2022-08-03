3ed4a52124366e1fc378f3f97d2f93f373c0f666
Patrick Whelan <patrickwhelan@gmx.com>
2022-08-03 22:13:44 +0100

    Added tests for distance utility and spawning projectiles

---
+++ Assets/Scripts/Tests/Playmode/Gameplay/FireProjectileTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Unity.Entities;
+using Unity.Transforms;
+
+public class FireProjectileTest : ECSTestsFixture
+{
+    [UnityTest]
+    public IEnumerator Test()
+    {
+        // Create our system to test
+        FireProjectileSystem System = m_World.GetOrCreateSystem<FireProjectileSystem>();
+
+        // 'Fake' some data and fire our projectile
+        Entity ToFire = System.EntityManager.CreateEntity(typeof(ProjectileTag));
+        FireProjectileSystem.FireProjectile(ToFire, new LocalToWorld(), 0);
+
+        // Wait a frame for the projectile to be spawned
+        yield return null;
+
+        // Assert that the projectile has been spawned
+        Assert.That(System.TryGetSingletonEntity<ProjectileTag>(out _));
+    }
+}
---
+++ Assets/Scripts/Tests/Playmode/Utility/Transforms.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.Mathematics;
+
+public class Transforms
+{
+    [Test]
+    public void DistanceTest()
+    {
+        float3 p1 = new float3(0, 0, 0);
+        float3 p2 = new float3(1, 0, 0);
+        float3 p3 = new float3(0, 2, 0);
+        float3 p4 = new float3(-3, 0, 3);
+
+        Assert.That(System.Math.Round(p1.Distance(p2), 2) == 1.0d);
+        Assert.That(System.Math.Round(p2.Distance(p3), 2) == 2.24d);
+        Assert.That(System.Math.Round(p3.Distance(p4), 2) == 4.69d);
+        Assert.That(System.Math.Round(p4.Distance(p1), 2) == 4.24d);
+    }
+}
