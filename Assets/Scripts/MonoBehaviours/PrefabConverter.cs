761c92176ddeff50a7dcad5b121da14726528a5d
patrickwhelan99 <patrickwhelan@gmx.com>
2022-10-05 13:31:23 +0100

    Fixed an issue spawning prefabs in unit tests. For some reason the conversionSettings constructor doesn't respect the provided destination world?

--- Assets/Scripts/MonoBehaviours/PrefabConverter.cs
+++ Assets/Scripts/MonoBehaviours/PrefabConverter.cs
@@ -10,7 +10,9 @@ public class PrefabConverter : Singleton<PrefabConverter>
         World DestWorld = DestinationWorld ?? World.DefaultGameObjectInjectionWorld;
 
         Instance.BlobStore ??= new BlobAssetStore();
-        Instance.Settings ??= GameObjectConversionSettings.FromWorld(DestWorld, Instance.BlobStore);
+        Instance.Settings ??= new GameObjectConversionSettings(DestWorld, GameObjectConversionUtility.ConversionFlags.IsBuildingForPlayer);
+
+        Instance.Settings.DestinationWorld = DestWorld;
 
         Entity E = GameObjectConversionUtility.ConvertGameObjectHierarchy(Obj, Instance.Settings);
 
